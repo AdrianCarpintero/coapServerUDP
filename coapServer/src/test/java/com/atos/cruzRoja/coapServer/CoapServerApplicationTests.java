92f595f56ba16e239f66fcfc2172919dd2366a10
AdrianCarpintero <64351919+AdrianCarpintero@users.noreply.github.com>
2023-02-26 18:27:35 +0100

    Add files via upload

---
+++ coapServer/src/test/java/com/atos/cruzRoja/coapServer/CoapServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atos.cruzRoja.coapServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoapServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
